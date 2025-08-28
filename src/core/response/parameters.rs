0a548752b8e05e9ec1b25b9341d77e7400b6388d
nodirbek.parpibaev <nodirbek.parpibaev@dsr-corporation.com>
2025-08-28 19:12:29 +0500

    ASDK-476: resolves some MR threads
    Signed-off-by: nodirbek.parpibaev <nodirbek.parpibaev@dsr-corporation.com>

--- src/core/response/parameters.rs
+++ src/core/response/parameters.rs
@@ -1,6 +1,7 @@
 pub use crate::core::authorization_request::parameters::State;
 use crate::core::object::TypedParameter;
 
+use crate::core::authorization_request::parameters::HashAlgorithm;
 use anyhow::{bail, Error};
 use serde::{Deserialize, Serialize};
 use serde_json::{Map, Value as Json};
@@ -11,7 +12,6 @@ use ssi::{
     prelude::{AnyDataIntegrity, AnyJsonPresentation, AnySuite, DataIntegrity},
     OneOrMany, JWK,
 };
-use crate::core::authorization_request::parameters::HashAlgorithm;
 
 #[derive(Debug, Clone)]
 pub struct IdToken {
